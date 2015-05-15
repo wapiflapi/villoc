bc3f089bdbe2a9a24551ed224495c165ed6e869c
dyjakan <dyjakan@sigsegv.pl>
2015-05-15 15:50:57 +0200

    example for tests

---
+++ dtracetool/tests/test1.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define DEBUG 0
+
+int 
+main(void)
+{
+    int *cptr, *mptr, *vptr;
+
+    cptr = calloc(8, 32);
+    if(DEBUG)
+        printf("cptr = %p\n", cptr);
+
+    mptr = malloc(64);
+    if(DEBUG)
+        printf("mptr = %p\n", mptr);
+
+    mptr = realloc(mptr, 128);
+    if(DEBUG)
+        printf("mptr = %p\n", mptr);
+
+    vptr = valloc(64);
+    if(DEBUG)
+        printf("vptr = %p\n", vptr);
+
+    vptr = reallocf(vptr, 128);
+    if(DEBUG)
+        printf("vptr = %p\n", vptr);
+
+    free(cptr);
+    free(mptr);
+    free(vptr);
+
+    return 0;
+}
+
