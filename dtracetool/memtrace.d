c75de68c0fbc3a15d1d7d04c81ad03ca4ff9f1c0
dyjakan <dyjakan@sigsegv.pl>
2015-06-29 23:06:56 +0200

    Major change. Adjusted to catch *alloc fails.

--- dtracetool/memtrace.d
+++ dtracetool/memtrace.d
@@ -20,75 +20,130 @@ to a certain degree via DTrace tuning [2] [3].
 /* Re-enable to see potential data drops */
 #pragma D option quiet
 
+/* Globals for alloc functions args. Explicitly typed to avoid errors. */
+size_t malloc_size;
+size_t valloc_size;
+size_t calloc_count;
+size_t calloc_size;
+int64_t realloc_addr;
+size_t realloc_size;
+int64_t reallocf_addr;
+size_t reallocf_size;
+int64_t free_addr;
+
+BEGIN
+{
+    /* Flags for failure inside of the functions */
+    /* However, we're using them also for predicates. This is a hack. */
+    malloc_fail = 0;
+    valloc_fail = 0;
+    calloc_fail = 0;
+    realloc_fail = 0;
+    reallocf_fail = 0;
+    free_fail = 0;
+}
+
 
 pid$target::malloc:entry
 {
-    self->msize = arg0;
+    malloc_size = arg0;
+    malloc_fail = 1;
 }
 
 pid$target::malloc:return
-/self->msize/
+/malloc_fail/
 {
-    printf("malloc(%d) = %#p\n", self->msize, arg1);
-    self->msize = 0;
+    printf("malloc(%d) = %#p\n", malloc_size, arg1);
+    malloc_fail = 0;
 }
 
 pid$target::valloc:entry
 {
-    self->vsize = arg0;
+    valloc_size = arg0;
+    valloc_fail = 1;
 }
 
 pid$target::valloc:return
-/self->vsize/
+/valloc_fail/
 {
-    printf("valloc(%d) = %#p\n", self->vsize, arg1);
-    self->vsize = 0;
+    printf("valloc(%d) = %#p\n", valloc_size, arg1);
+    valloc_fail = 0;
 }
 
 pid$target::calloc:entry
 {
-    self->ccount = arg0;
-    self->csize = arg1;
+    calloc_count = arg0;
+    calloc_size = arg1;
+    calloc_fail = 1;
 }
 
 pid$target::calloc:return
-/self->csize/
+/calloc_fail/
 {
-    printf("calloc(%d, %d) = %#p\n", self->ccount, self->csize, arg1);
-    self->ccount = 0;
-    self->csize = 0;
+    printf("calloc(%d, %d) = %#p\n", calloc_count, calloc_size, arg1);
+    calloc_fail = 0;
 }
 
 pid$target::realloc:entry
 {
-    self->raddr = arg0;
-    self->rsize = arg1;
+    realloc_addr = arg0;
+    realloc_size = arg1;
+    realloc_fail = 1;
 }
 
 pid$target::realloc:return
-/self->rsize/
+/realloc_fail/
 {
-    printf("realloc(%#p, %d) = %#p\n", self->raddr, self->rsize, arg1);
-    self->rsize = 0;
-    self->raddr = 0;
+    printf("realloc(%#p, %d) = %#p\n", realloc_addr, realloc_size, arg1);
+    realloc_fail = 0;
 }
 
 pid$target::reallocf:entry
 {
-    self->rfaddr = arg0;
-    self->rfsize = arg1;
+    reallocf_addr = arg0;
+    reallocf_size = arg1;
+    reallocf_fail = 1;
 }
 
 pid$target::reallocf:return
-/self->rfsize/
+/reallocf_fail/
 {
-    printf("reallocf(%#p, %d) = %#p\n", self->rfaddr, self->rfsize, arg1);
-    self->rfaddr = 0;
-    self->rfsize = 0;
+    printf("reallocf(%#p, %d) = %#p\n", reallocf_addr, reallocf_size, arg1);
+    reallocf_fail = 0;
 }
 
 pid$target::free:entry
 {
-    printf("free(%#p) = <void>\n", arg0);
+    free_addr = arg0;
+    printf("free(%#p) = <void>\n", free_addr);
 }
 
+END
+/malloc_fail/
+{
+    printf("malloc(%d) = <error>\n", malloc_size);
+}
+
+END
+/valloc_fail/
+{
+    printf("valloc(%d) = <error>\n", valloc_size);
+}
+
+END
+/calloc_fail/
+{
+    printf("calloc(%d, %d) = <error>\n", calloc_count, calloc_size);
+}
+
+END
+/realloc_fail/
+{
+    printf("realloc(%#p, %d) = <error>\n", realloc_addr, realloc_size);
+}
+
+END
+/reallocf_fail/
+{
+    printf("realloc(%#p, %d) = <error>\n", reallocf_addr, reallocf_size);
+}
